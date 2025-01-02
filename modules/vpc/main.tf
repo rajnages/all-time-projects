38b3110ba9fa128182de8e91d26cd0ebd1a9bfe3
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2025-01-02 10:17:55 +0530

    Changes are Done

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main_vpc.id
   tags = {
-    Name = "${var.vpc_name}-igw"
+    Name = "${terraform.workspace}-igw"
   }
 }
 
