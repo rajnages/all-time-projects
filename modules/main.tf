ad3c7f9c3071dcfea763d5032d0a97eb9cf54800
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2025-01-02 09:45:09 +0530

    GitHub Actions changed

--- modules/main.tf
+++ modules/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "terraform-environments-workspaces"
-    key    = "environment/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
